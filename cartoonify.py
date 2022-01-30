d2c0db0d1fb47294bd0a0dbc92ac4d61474bc745
Alankrit.sahai <79023366+Alankrit-prog@users.noreply.github.com>
2022-01-31 01:07:50 +0530

    Update cartoonify.py

--- cartoonify.py
+++ cartoonify.py
@@ -10,7 +10,7 @@ image = cv2.imread(r"C:\Users\Lenovo\Desktop\t5mex3dyn30xi3ox6ii5.jpg")
 
 if(image is not None):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.medianBlur(gray, 1)
+    gray = cv2.medianBlur(gray, 2)
     edges = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
     cv2.THRESH_BINARY, 11, 11)
     color = cv2.bilateralFilter(image, 9, 250, 250)
